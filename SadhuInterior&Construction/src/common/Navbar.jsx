8ef303ab4db782a6c0cc5c57907610858c164f67
guptak-hash <guptakishan1123@gmail.com>
2025-03-03 13:57:27 +0530

    provided routes

--- SadhuInterior&Construction/src/common/Navbar.jsx
+++ SadhuInterior&Construction/src/common/Navbar.jsx
@@ -3,7 +3,9 @@ import {Link, NavLink} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='navbar-container'>
+        <div className='logo'>
         <Link to='/'>LOGO</Link>
+        </div>
         <div className='nav-links'>
            <NavLink to='/about'>ABOUT</NavLink>
            <NavLink to='/services'>SERVICES</NavLink>
